914582b15b77d12fd5c220fed3f276851f7d35a2
Ryan Parman <ryan@ryanparman.com>
2022-10-03 10:57:27 -0600

    Update aws-okta.rb

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -5,7 +5,7 @@ class AwsOkta < Formula
   desc "aws-vault like tool for Okta authentication"
   homepage "https://github.com/skyzyx/aws-okta/"
   url "https://github.com/skyzyx/aws-okta/archive/refs/heads/master.tar.gz"
-  sha256 "444a84cd9c81097a7c462f806605193c5676879133255cfa0f610b7d14756b65"
+  # sha256 "444a84cd9c81097a7c462f806605193c5676879133255cfa0f610b7d14756b65"
 
   depends_on "go" => :build
 
