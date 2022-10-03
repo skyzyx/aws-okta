253d8188b1eafd987b6b2a1ee7702801e1a5bbcf
Ryan Parman <ryan@ryanparman.com>
2022-10-03 11:28:17 -0600

    Update aws-okta.rb

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -9,7 +9,11 @@ class AwsOkta < Formula
   depends_on "go" => :build
 
   def install
-    system "make"
+    ENV["VERSION"]=shell_output('git describe --tags --always --dirty="-dev"')
+    ENV["GOOS"]="darwin"
+    ENV["GOARCH"]="amd64"
+    ENV["GO111MODULE"]="on"
+    system "go build -mod=vendor -o aws-okta"
   end
 
   test do
